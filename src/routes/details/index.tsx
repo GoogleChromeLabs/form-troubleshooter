0d9df52888fb8bc4a7c2f1e84ac66a9cff451183
Soc Sieng <socsieng@google.com>
2021-08-05 11:56:50 -0700

    fix: include fields without forms in details page

--- src/routes/details/index.tsx
+++ src/routes/details/index.tsx
@@ -1,13 +1,14 @@
 import { Fragment, FunctionalComponent, h } from 'preact';
 import Table from '../../components/table';
 import { stringifyFormElement } from '../../lib/audits/audit-util';
-import { findDescendants, getTextContent } from '../../lib/tree-util';
+import { closestParent, findDescendants, getTextContent } from '../../lib/tree-util';
 import {
   handleHighlightClick,
   handleHighlightMouseEnter,
   handleHighlightMouseLeave,
 } from '../../lib/element-highlighter';
 import style from './style.css';
+import { groupBy } from '../../lib/array-util';
 
 interface Props {
   documentTree: TreeNodeWithParent | undefined;
@@ -15,118 +16,139 @@ interface Props {
 
 const Details: FunctionalComponent<Props> = props => {
   const { documentTree: tree } = props;
-  let forms: TreeNodeWithParent[] = [];
+  let forms: Map<TreeNodeWithParent | null, Map<string, TreeNodeWithParent[]>> = new Map();
   const formSections = [
     {
       name: 'Inputs',
-      getItems(form: TreeNodeWithParent) {
-        return findDescendants(form, ['input']).map(field => ({
-          ...field.attributes,
-          required: field.attributes.required !== undefined ? 'true' : '',
-          _field: field,
-        }));
+      type: 'input',
+      toItem(item: TreeNodeWithParent) {
+        return {
+          ...item.attributes,
+          required: item.attributes.required !== undefined ? 'true' : '',
+          _field: item,
+        };
       },
       columns: ['id', 'name', 'class', 'type', 'autocomplete', 'placeholder', 'required'],
     },
     {
       name: 'Selects',
-      getItems(form: TreeNodeWithParent) {
-        return findDescendants(form, ['select']).map(field => ({
-          ...field.attributes,
-          required: field.attributes.required !== undefined ? 'true' : '',
-          _field: field,
-        }));
+      type: 'select',
+      toItem(item: TreeNodeWithParent) {
+        return {
+          ...item.attributes,
+          required: item.attributes.required !== undefined ? 'true' : '',
+          _field: item,
+        };
       },
       columns: ['id', 'name', 'class', 'placeholder', 'required'],
     },
     {
       name: 'Text areas',
-      getItems(form: TreeNodeWithParent) {
-        return findDescendants(form, ['textarea']).map(field => ({
-          ...field.attributes,
-          required: field.attributes.required !== undefined ? 'true' : '',
-          _field: field,
-        }));
+      type: 'textarea',
+      toItem(item: TreeNodeWithParent) {
+        return {
+          ...item.attributes,
+          required: item.attributes.required !== undefined ? 'true' : '',
+          _field: item,
+        };
       },
       columns: ['id', 'name', 'class', 'autocomplete', 'placeholder', 'required'],
     },
     {
       name: 'Buttons',
-      getItems(form: TreeNodeWithParent) {
-        return findDescendants(form, ['button']).map(field => ({
-          ...field.attributes,
-          text: getTextContent(field),
-          _field: field,
-        }));
+      type: 'button',
+      toItem(item: TreeNodeWithParent) {
+        return {
+          ...item.attributes,
+          text: getTextContent(item),
+          _field: item,
+        };
       },
       columns: ['id', 'name', 'class', 'text', 'type'],
     },
     {
       name: 'Labels',
-      getItems(form: TreeNodeWithParent) {
-        return findDescendants(form, ['label']).map(field => ({
-          ...field.attributes,
-          text: getTextContent(field),
-          _field: field,
-        }));
+      type: 'label',
+      toItem(item: TreeNodeWithParent) {
+        return {
+          ...item.attributes,
+          text: getTextContent(item),
+          _field: item,
+        };
       },
       columns: ['id', 'name', 'class', 'for', 'text'],
     },
   ];
 
   if (tree) {
-    forms = findDescendants(tree, ['form']);
+    forms = new Map(
+      Array.from(
+        groupBy(
+          findDescendants(
+            tree,
+            formSections.map(section => section.type),
+          ),
+          item => closestParent(item, 'form'),
+        ).entries(),
+      ).map(([form, fields]) => [form, groupBy(fields, field => field.name!)]),
+    );
   }
 
   return (
     <div class={style.details}>
-      {forms.map((form, formIndex) => (
-        <Fragment key={formIndex}>
-          <h3>
-            Form:{' '}
-            <a
-              onClick={() => {
-                handleHighlightClick(form);
-              }}
-              onMouseEnter={() => {
-                handleHighlightMouseEnter(form);
-              }}
-              onMouseLeave={() => {
-                handleHighlightMouseLeave(form);
-              }}
-            >
-              <code>{stringifyFormElement(form)}</code>
-            </a>
-          </h3>
-          {formSections.map((section, sectionIndex) => {
-            const items = section.getItems(form);
-            if (!items.length) {
-              return null;
-            }
+      {Array.from(forms.entries())
+        .sort(([key1], [key2]) => (`${key1}` < `${key2}` ? -1 : 0))
+        .map(([form, fieldsMap], formIndex) => (
+          <Fragment key={formIndex}>
+            {form ? (
+              <h3>
+                Form:{' '}
+                <a
+                  onClick={() => {
+                    handleHighlightClick(form);
+                  }}
+                  onMouseEnter={() => {
+                    handleHighlightMouseEnter(form);
+                  }}
+                  onMouseLeave={() => {
+                    handleHighlightMouseLeave(form);
+                  }}
+                >
+                  <code>{stringifyFormElement(form)}</code>
+                </a>
+              </h3>
+            ) : (
+              <h3>Without a form</h3>
+            )}
+            {formSections.map((section, sectionIndex) => {
+              const items = fieldsMap.get(section.type)?.map(field => section.toItem(field));
+              if (!items?.length) {
+                return null;
+              }
 
-            return (
-              <Fragment key={sectionIndex}>
-                <h4>
-                  {section.name} ({items.length})
-                </h4>
-                <Table
-                  items={items}
-                  columns={section.columns}
-                  onRowClick={(row, item) => {
-                    handleHighlightClick(item._field);
-                  }}
-                  onRowEnter={(row, item) => {
-                    handleHighlightMouseEnter(item._field);
-                  }}
-                  onRowLeave={(row, item) => {
-                    handleHighlightMouseLeave(item._field);
-                  }}
-                />
-              </Fragment>
-            );
-          })}
-        </Fragment>
-      ))}
+              return (
+                <Fragment key={sectionIndex}>
+                  <h4>
+                    {section.name} ({items.length})
+                  </h4>
+                  <Table
+                    items={items}
+                    columns={section.columns}
+                    onRowClick={(row, item) => {
+                      handleHighlightClick(item._field);
+                    }}
+                    onRowEnter={(row, item) => {
+                      handleHighlightMouseEnter(item._field);
+                    }}
+                    onRowLeave={(row, item) => {
+                      handleHighlightMouseLeave(item._field);
+                    }}
+                  />
+                </Fragment>
+              );
+            })}
+          </Fragment>
+        ))}
     </div>
   );
 };
