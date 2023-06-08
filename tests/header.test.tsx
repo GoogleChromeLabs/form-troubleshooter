b91a86afb4a7a78dbdfc019005587c571c297819
Sanjoy Ghosh <sanjoyg@google.com>
2023-06-08 02:25:09 -0700

    Fix the header.test.tsx using the Preact Test Library

--- tests/header.test.tsx
+++ tests/header.test.tsx
@@ -4,11 +4,11 @@ SPDX-License-Identifier: Apache-2.0 */
 import { h } from 'preact';
 import Header from '../src/components/header';
 // See: https://github.com/preactjs/enzyme-adapter-preact-pure
-import { render, fireEvent, screen, waitFor } from '@testing-library/preact';
+import { render } from '@testing-library/preact';
 
 describe('Initial Test of the Header', function () {
   test('Header renders', function () {
     const dom = render(<Header />);
-    expect(dom.container.getElementsByTagName("header")[0].textContent).toBe('Form troubleshooter');
+    expect(dom.container.getElementsByTagName('header')[0].textContent).toBe('Form troubleshooter');
   });
 });
