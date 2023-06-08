f79cfc768b00886f379f60c63228eb795cb0ea2d
Sanjoy Ghosh <sanjoyg@google.com>
2023-06-08 13:49:10 -0700

    Revive the CodeWrap tests

--- tests/code-wrap.test.tsx
+++ tests/code-wrap.test.tsx
@@ -3,10 +3,10 @@ SPDX-License-Identifier: Apache-2.0 */
 
 import { h } from 'preact';
 // See: https://github.com/preactjs/enzyme-adapter-preact-pure
-import { shallow, ShallowWrapper } from 'enzyme';
+import { configure, shallow, ShallowWrapper } from 'enzyme';
 import CodeWrap from '../src/components/code-wrap';
-import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-preact-pure';
+import { ReactElement } from 'react';
 
 configure({ adapter: new Adapter() });
 
@@ -17,43 +17,40 @@ function getCodeInfo(context: ShallowWrapper) {
     .map(elem => ({ text: elem.text(), type: elem.type() }));
 }
 
-/*
 describe('CodeWrap', function () {
   test('Wraps a string in code', function () {
-    const context = shallow(<CodeWrap text="hello" />);
+    const context = shallow((<CodeWrap text="hello" />) as unknown as ReactElement);
     expect(getCodeInfo(context)).toEqual([{ text: 'hello' }]);
   });
 
   test('Wraps a string in code when text not found', function () {
-    const context = shallow(React.createElement('div', {}, <CodeWrap text="hello" emphasize="boo" />));
+    const context = shallow((<CodeWrap text="hello" emphasize="boo" />) as unknown as ReactElement);
     expect(getCodeInfo(context)).toEqual([{ text: 'hello' }]);
   });
 
   test('Wraps a string in code with emphasis (beginning of text)', function () {
-    const context = shallow(React.createElement('div', {}, <CodeWrap text="hello world" emphasize="hello" />));
+    const context = shallow((<CodeWrap text="hello world" emphasize="hello" />) as unknown as ReactElement);
     expect(getCodeInfo(context)).toEqual([{ text: 'hello', type: 'strong' }, { text: ' world' }]);
   });
 
   test('Wraps a string in code with emphasis (middle of text)', function () {
-    const context = shallow(React.createElement('div', {}, <CodeWrap text="hello world" emphasize="lo wo" />));
+    const context = shallow((<CodeWrap text="hello world" emphasize="lo wo" />) as unknown as ReactElement);
     expect(getCodeInfo(context)).toEqual([{ text: 'hel' }, { text: 'lo wo', type: 'strong' }, { text: 'rld' }]);
   });
 
   test('Wraps a string in code with emphasis (end of text)', function () {
-    const context = shallow(React.createElement('div', {}, <CodeWrap text="hello world" emphasize="world" />));
+    const context = shallow((<CodeWrap text="hello world" emphasize="world" />) as unknown as ReactElement);
     expect(getCodeInfo(context)).toEqual([{ text: 'hello ' }, { text: 'world', type: 'strong' }]);
   });
 
   test('Wraps a string in code with emphasis on first instances', function () {
-    const context = shallow(
-      React.createElement('div', {}, <CodeWrap text="hello world hello hello" emphasize="hello" />),
-    );
+    const context = shallow((<CodeWrap text="hello world hello hello" emphasize="hello" />) as unknown as ReactElement);
     expect(getCodeInfo(context)).toEqual([{ text: 'hello', type: 'strong' }, { text: ' world hello hello' }]);
   });
 
   test('Wraps a string in code with emphasis on all instances', function () {
     const context = shallow(
-      React.createElement('div', {}, <CodeWrap text="hello world hello hello" emphasize={/hello/g} />),
+      (<CodeWrap text="hello world hello hello" emphasize={/hello/g} />) as unknown as ReactElement,
     );
     expect(getCodeInfo(context)).toEqual([
       { text: 'hello', type: 'strong' },
@@ -66,14 +63,9 @@ describe('CodeWrap', function () {
 
   test('Wraps a string in code with emphasis on a full regular expression', function () {
     const context = shallow(
-      React.createElement(
-        'div',
-        {},
-        <CodeWrap
-          text='<input class="StripeField--fake" autocomplete="fake" ...>'
-          emphasize={/autocomplete="[^"]*"/}
-        />,
-      ),
+      (
+        <CodeWrap text='<input class="StripeField--fake" autocomplete="fake" ...>' emphasize={/autocomplete="[^"]*"/} />
+      ) as unknown as ReactElement,
     );
     expect(getCodeInfo(context)).toEqual([
       { text: '<input class="StripeField--fake" ' },
@@ -84,14 +76,12 @@ describe('CodeWrap', function () {
 
   test('Wraps a string in code with emphasis on the first capture group of a regular expression', function () {
     const context = shallow(
-      React.createElement(
-        'div',
-        {},
+      (
         <CodeWrap
           text='<input class="StripeField--fake" autocomplete="fake" ...>'
           emphasize={/autocomplete="([^"]*)"/}
-        />,
-      ),
+        />
+      ) as unknown as ReactElement,
     );
     expect(getCodeInfo(context)).toEqual([
       { text: '<input class="StripeField--fake" autocomplete="' },
@@ -102,11 +92,9 @@ describe('CodeWrap', function () {
 
   test('Wraps a string in code with emphasis on the first capture group of a global regular expression', function () {
     const context = shallow(
-      React.createElement(
-        'div',
-        {},
-        <CodeWrap text='<input class="StripeField--fake" autocomplete="fake" ...>' emphasize={/\b(\w+)="([^"]*)"/g} />,
-      ),
+      (
+        <CodeWrap text='<input class="StripeField--fake" autocomplete="fake" ...>' emphasize={/\b(\w+)="([^"]*)"/g} />
+      ) as unknown as ReactElement,
     );
     expect(getCodeInfo(context)).toEqual([
       { text: '<input ' },
@@ -119,7 +107,7 @@ describe('CodeWrap', function () {
 
   test('Wraps a string in code with multiple words to emphasize', function () {
     const context = shallow(
-      React.createElement('div', {}, <CodeWrap text="hello world" emphasize={['world', 'hello']} />),
+      (<CodeWrap text="hello world" emphasize={['world', 'hello']} />) as unknown as ReactElement,
     );
     expect(getCodeInfo(context)).toEqual([
       { text: 'hello', type: 'strong' },
@@ -129,33 +117,23 @@ describe('CodeWrap', function () {
   });
 
   test('Wraps a string in code with multiple adjacent words to emphasize', function () {
-    const context = shallow(
-      React.createElement('div', {}, <CodeWrap text="helloworld" emphasize={['world', 'hello']} />),
-    );
+    const context = shallow((<CodeWrap text="helloworld" emphasize={['world', 'hello']} />) as unknown as ReactElement);
     expect(getCodeInfo(context)).toEqual([{ text: 'helloworld', type: 'strong' }]);
   });
 
   test('Wraps a string in code with multiple words to emphasize with one word sharing a prefix with another', function () {
-    const context = shallow(
-      React.createElement('div', {}, <CodeWrap text="hello world" emphasize={['hell', 'hello']} />),
-    );
+    const context = shallow((<CodeWrap text="hello world" emphasize={['hell', 'hello']} />) as unknown as ReactElement);
     expect(getCodeInfo(context)).toEqual([{ text: 'hello', type: 'strong' }, { text: ' world' }]);
   });
 
   test('Wraps a string in code with multiple words to emphasize with one word sharing a suffix with another', function () {
-    const context = shallow(
-      React.createElement('div', {}, <CodeWrap text="hello world" emphasize={['ello', 'hello']} />),
-    );
+    const context = shallow((<CodeWrap text="hello world" emphasize={['ello', 'hello']} />) as unknown as ReactElement);
     expect(getCodeInfo(context)).toEqual([{ text: 'hello', type: 'strong' }, { text: ' world' }]);
   });
 
   test('Wraps a string in code with multiple words to emphasize with one word sharing a prefix with another, and one being global', function () {
     const context = shallow(
-      React.createElement(
-        'div',
-        {},
-        <CodeWrap text="hello world, hello, hello, hello" emphasize={[/hell/g, 'hello']} />,
-      ),
+      (<CodeWrap text="hello world, hello, hello, hello" emphasize={[/hell/g, 'hello']} />) as unknown as ReactElement,
     );
     expect(getCodeInfo(context)).toEqual([
       { text: 'hello', type: 'strong' },
@@ -178,11 +156,7 @@ describe('CodeWrap', function () {
 
     test('Wraps an attribute with a value', function () {
       const context = shallow(
-        React.createElement(
-          'div',
-          {},
-          <CodeWrap text={'<a class="some link here" link="yes">'} emphasize={expression} />,
-        ),
+        (<CodeWrap text={'<a class="some link here" link="yes">'} emphasize={expression} />) as unknown as ReactElement,
       );
       expect(getCodeInfo(context)).toEqual([
         { text: '<a class="some link here" ' },
@@ -193,7 +167,7 @@ describe('CodeWrap', function () {
 
     test('Wraps an attribute without a value at the end of a tag', function () {
       const context = shallow(
-        React.createElement('div', {}, <CodeWrap text={'<a class="some link here" link>'} emphasize={expression} />),
+        (<CodeWrap text={'<a class="some link here" link>'} emphasize={expression} />) as unknown as ReactElement,
       );
       expect(getCodeInfo(context)).toEqual([
         { text: '<a class="some link here" ' },
@@ -204,11 +178,7 @@ describe('CodeWrap', function () {
 
     test('Wraps an attribute without a value with ...', function () {
       const context = shallow(
-        React.createElement(
-          'div',
-          {},
-          <CodeWrap text={'<a class="some link here" link ...>'} emphasize={expression} />,
-        ),
+        (<CodeWrap text={'<a class="some link here" link ...>'} emphasize={expression} />) as unknown as ReactElement,
       );
       expect(getCodeInfo(context)).toEqual([
         { text: '<a class="some link here" ' },
@@ -219,11 +189,9 @@ describe('CodeWrap', function () {
 
     test('Wraps an attribute without a value with other attributes trailing', function () {
       const context = shallow(
-        React.createElement(
-          'div',
-          {},
-          <CodeWrap text={'<a class="some link here" link rel="noopener">'} emphasize={expression} />,
-        ),
+        (
+          <CodeWrap text={'<a class="some link here" link rel="noopener">'} emphasize={expression} />
+        ) as unknown as ReactElement,
       );
       expect(getCodeInfo(context)).toEqual([
         { text: '<a class="some link here" ' },
@@ -233,4 +201,3 @@ describe('CodeWrap', function () {
     });
   });
 });
-*/
